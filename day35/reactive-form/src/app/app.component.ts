9a5da6bec6986065d626ce2236a0c5bfe0622abc
NIKHILPAWAR96 <nikpawar96@gmail.com>
2022-04-11 23:33:51 +0530

    angular

---
+++ day35/reactive-form/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'reactive-form';
+  firstname = new FormControl;
+  lastname = new FormControl;
+
+}
